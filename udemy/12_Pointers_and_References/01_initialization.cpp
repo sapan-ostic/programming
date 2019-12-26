2db4c867801936b959e25bc69da8d54d8786e4f3
sapan-ostic <sapanagrawal777@gmail.com>
2019-12-26 18:23:09 -0500

    testing ssh keys

--- udemy/12_Pointers_and_References/01_initialization.cpp
+++ udemy/12_Pointers_and_References/01_initialization.cpp
@@ -5,9 +5,9 @@ int main(){
     float *float_ptr {nullptr};
     char *char_ptr;
 
-    std::cout<< *int_ptr << std::endl;
-    std::cout<< *float_ptr << std::endl;
-    std::cout<< *char_ptr << std::endl;
+    // std::cout<< *int_ptr << std::endl;
+    // std::cout<< *float_ptr << std::endl;
+    // std::cout<< *char_ptr << std::endl;
 
     return 0;
 }
