d4b5731a21d405120974c49f005eb818f7f21fe0
sapan-ostic <sapanagrawal777@gmail.com>
2020-01-21 14:20:52 -0500

    general commit

--- algorithms/linked_list.cpp
+++ algorithms/linked_list.cpp
@@ -10,6 +10,7 @@ struct Node{
 void insertAtEnd(Node **headptr, int val);
 void insertAtBegin(Node **headptr, int val);
 void insert(Node **headptr, int pos, int val);
+void deleteNode(Node **headptr, int pos);
 void printNode(Node *head);
 
 int main(){
@@ -20,6 +21,7 @@ int main(){
     insertAtBegin(&head, 3); // 3->2->1->0->nullptr
     insert(&head, 2, 4); // 3->2->4->1->0->nullptr
     insertAtEnd(&head, 10); // 3->2->4->1->0->10->nullptr
+    deleteNode(&head, 2); // 3->2->1->0->10->nullptr
     printNode(head);
     return 0; 
 }
@@ -57,6 +59,16 @@ void insert(Node **headptr, int pos, int val){
         temp->next = newNode;
         newNode->value = val;
     }    
+}
+
+void deleteNode(Node **headptr, int pos){
+    Node * temp = *headptr;
+    for (int i {0}; i<pos-1; i++){
+        temp = temp->next;
+    }
+    Node * temp2 = temp->next;
+    temp->next = temp2->next;
+    delete temp2;
 }
 
 void printNode(Node *head){
