4a17be4549392f0e009d881f57112c14aae6657e
sapan-ostic <sapanagrawal777@gmail.com>
2022-03-24 22:14:25 -0700

    Final changes for profiler instrumentor

--- include/Instrumentor.h
+++ include/Instrumentor.h
@@ -1,147 +1,143 @@
 //
 // Basic instrumentation profiler by Cherno
 
-// Usage: include this header file somewhere in your code (eg. precompiled header), and then use like:
+// Usage: include this header file somewhere in your code (eg. precompiled
+// header), and then use like:
 //
-// Instrumentor::Get().BeginSession("Session Name");        // Begin session 
+// Instrumentor::Get().BeginSession("Session Name");        // Begin session
 // {
-//     InstrumentationTimer timer("Profiled Scope Name");   // Place code like this in scopes you'd like to include in profiling
+//     InstrumentationTimer timer("Profiled Scope Name");   // Place code like
+//     this in scopes you'd like to include in profiling
 //     // Code
 // }
 // Instrumentor::Get().EndSession();                        // End Session
 //
-// You will probably want to macro-fy this, to switch on/off easily and use things like __FUNCSIG__ for the profile name.
+// You will probably want to macro-fy this, to switch on/off easily and use
+// things like __FUNCSIG__ for the profile name.
 //
 #pragma once
 
-#include <string>
-#include <chrono>
 #include <algorithm>
+#include <chrono>
 #include <fstream>
+#include <string>
 
 #include <mutex>
 #include <thread>
 
-struct ProfileResult
-{
-    std::string Name;
-    long long Start, End;
-    uint32_t ThreadID;
+struct ProfileResult {
+  std::string Name;
+  long long Start, End;
+  uint32_t ThreadID;
 };
 
-struct InstrumentationSession
-{
-    std::string Name;
+struct InstrumentationSession {
+  std::string Name;
 };
 
-class Instrumentor
-{
+class Instrumentor {
 private:
-    InstrumentationSession* m_CurrentSession;
-    std::ofstream m_OutputStream;
-    int m_ProfileCount;
-    std::mutex m_lock;
+  InstrumentationSession *m_CurrentSession;
+  std::ofstream m_OutputStream;
+  int m_ProfileCount;
+  std::mutex m_lock;
+
 public:
-    Instrumentor()
-        : m_CurrentSession(nullptr), m_ProfileCount(0)
-    {
-    }
+  Instrumentor() : m_CurrentSession(nullptr), m_ProfileCount(0) {}
 
-    void BeginSession(const std::string& name, const std::string& filepath = "results.json")
-    {
-        m_OutputStream.open(filepath);
-        WriteHeader();
-        m_CurrentSession = new InstrumentationSession{ name };
-    }
+  void BeginSession(const std::string &name,
+                    const std::string &filepath = "results.json") {
+    m_OutputStream.open(filepath);
+    WriteHeader();
+    m_CurrentSession = new InstrumentationSession{name};
+  }
 
-    void EndSession()
-    {
-        WriteFooter();
-        m_OutputStream.close();
-        delete m_CurrentSession;
-        m_CurrentSession = nullptr;
-        m_ProfileCount = 0;
-    }
+  void EndSession() {
+    WriteFooter();
+    m_OutputStream.close();
+    delete m_CurrentSession;
+    m_CurrentSession = nullptr;
+    m_ProfileCount = 0;
+  }
 
-    void WriteProfile(const ProfileResult& result)
-    {
-        std::lock_guard<std::mutex> lock(m_lock);
+  void WriteProfile(const ProfileResult &result) {
+    std::lock_guard<std::mutex> lock(m_lock);
 
-        if (m_ProfileCount++ > 0)
-            m_OutputStream << ",";
+    if (m_ProfileCount++ > 0)
+      m_OutputStream << ",";
 
-        std::string name = result.Name;
-        std::replace(name.begin(), name.end(), '"', '\'');
+    std::string name = result.Name;
+    std::replace(name.begin(), name.end(), '"', '\'');
 
-        m_OutputStream << "{";
-        m_OutputStream << "\"cat\":\"function\",";
-        m_OutputStream << "\"dur\":" << (result.End - result.Start) << ',';
-        m_OutputStream << "\"name\":\"" << name << "\",";
-        m_OutputStream << "\"ph\":\"X\",";
-        m_OutputStream << "\"pid\":0,";
-        m_OutputStream << "\"tid\":" << result.ThreadID << ",";
-        m_OutputStream << "\"ts\":" << result.Start;
-        m_OutputStream << "}";
+    m_OutputStream << "{";
+    m_OutputStream << "\"cat\":\"function\",";
+    m_OutputStream << "\"dur\":" << (result.End - result.Start) << ',';
+    m_OutputStream << "\"name\":\"" << name << "\",";
+    m_OutputStream << "\"ph\":\"X\",";
+    m_OutputStream << "\"pid\":0,";
+    m_OutputStream << "\"tid\":" << result.ThreadID << ",";
+    m_OutputStream << "\"ts\":" << result.Start;
+    m_OutputStream << "}";
 
-        m_OutputStream.flush();
-    }
+    m_OutputStream.flush();
+  }
 
-    void WriteHeader()
-    {
-        m_OutputStream << "{\"otherData\": {},\"traceEvents\":[";
-        m_OutputStream.flush();
-    }
+  void WriteHeader() {
+    m_OutputStream << "{\"otherData\": {},\"traceEvents\":[";
+    m_OutputStream.flush();
+  }
 
-    void WriteFooter()
-    {
-        m_OutputStream << "]}";
-        m_OutputStream.flush();
-    }
+  void WriteFooter() {
+    m_OutputStream << "]}";
+    m_OutputStream.flush();
+  }
 
-    static Instrumentor& Get()
-    {
-        static Instrumentor* instance = new Instrumentor();
-        return *instance;
-    }
+  static Instrumentor &Get() {
+    static Instrumentor *instance = new Instrumentor();
+    return *instance;
+  }
 };
 
-class InstrumentationTimer
-{
+class InstrumentationTimer {
 public:
-    InstrumentationTimer(const char* name)
-        : m_Name(name), m_Stopped(false)
-    {
-        m_StartTimepoint = std::chrono::high_resolution_clock::now();
-    }
+  InstrumentationTimer(const char *name) : m_Name(name), m_Stopped(false) {
+    m_StartTimepoint = std::chrono::high_resolution_clock::now();
+  }
 
-    ~InstrumentationTimer()
-    {
-        if (!m_Stopped)
-            Stop();
-    }
+  ~InstrumentationTimer() {
+    if (!m_Stopped)
+      Stop();
+  }
 
-    void Stop()
-    {
-        auto endTimepoint = std::chrono::high_resolution_clock::now();
+  void Stop() {
+    auto endTimepoint = std::chrono::high_resolution_clock::now();
 
-        long long start = std::chrono::time_point_cast<std::chrono::microseconds>(m_StartTimepoint).time_since_epoch().count();
-        long long end = std::chrono::time_point_cast<std::chrono::microseconds>(endTimepoint).time_since_epoch().count();
+    long long start = std::chrono::time_point_cast<std::chrono::microseconds>(
+                          m_StartTimepoint)
+                          .time_since_epoch()
+                          .count();
+    long long end =
+        std::chrono::time_point_cast<std::chrono::microseconds>(endTimepoint)
+            .time_since_epoch()
+            .count();
 
-        uint32_t threadID = std::hash<std::thread::id>{}(std::this_thread::get_id());
-        Instrumentor::Get().WriteProfile({ m_Name, start, end, threadID });
+    uint32_t threadID =
+        std::hash<std::thread::id>{}(std::this_thread::get_id());
+    Instrumentor::Get().WriteProfile({m_Name, start, end, threadID});
 
-        m_Stopped = true;
-    }
+    m_Stopped = true;
+  }
+
 private:
-    const char* m_Name;
-    std::chrono::time_point<std::chrono::high_resolution_clock> m_StartTimepoint;
-    bool m_Stopped;
+  const char *m_Name;
+  std::chrono::time_point<std::chrono::high_resolution_clock> m_StartTimepoint;
+  bool m_Stopped;
 };
 
 #define PROFILING 1
 #ifdef PROFILING
-  #define PROFILE_SCOPE(name) InstrumentationTimer timer##__LINE__(name)
-  #define PROFILE_FUNCTION() PROFILE_SCOPE(__PRETTY_FUNCTION__)
+#define PROFILE_SCOPE(name) InstrumentationTimer timer##__LINE__(name)
+#define PROFILE_FUNCTION() PROFILE_SCOPE(__PRETTY_FUNCTION__)
 #else
-  #define PROFILE_SCOPE(name)
+#define PROFILE_SCOPE(name)
 #endif
